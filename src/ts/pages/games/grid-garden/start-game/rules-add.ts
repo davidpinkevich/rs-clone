8013b73271c4093368eec13974583f86a7b83a5a
davidpinkevich <davidpinkevich95@gmail.com>
2023-02-03 19:12:23 +0300

    refactor: fix path

--- src/ts/pages/games/grid-garden/start-game/rules-add.ts
+++ src/ts/pages/games/grid-garden/start-game/rules-add.ts
@@ -4,6 +4,7 @@ const addRules = () => {
   const header = <HTMLElement>document.querySelector(".garden__info-rules");
 
   header.innerHTML = `${RULES_GARDEN.RULES[1]}`;
+  console.log(header);
 };
 
 export default addRules;
