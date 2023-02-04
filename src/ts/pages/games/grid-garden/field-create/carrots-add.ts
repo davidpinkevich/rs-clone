83789cadb8cbc277c76278170875787a589f8fe9
davidpinkevich <davidpinkevich95@gmail.com>
2023-02-04 22:38:19 +0300

    feat: add image for carrot and drow for next level

--- src/ts/pages/games/grid-garden/field-create/carrots-add.ts
+++ src/ts/pages/games/grid-garden/field-create/carrots-add.ts
@@ -1,11 +1,25 @@
 import RULES_GARDEN from "../data/rules";
+import LocalStorage from "../../../../utils/LocalStorage";
 
 const addCarrots = () => {
   const field = <HTMLElement>document.querySelector(".field__carrots");
 
   const carrot = document.createElement("div");
   carrot.classList.add("field__carrots-item");
-  carrot.setAttribute("style", `${RULES_GARDEN.CARROT_RULES[2]}`);
+
+  const svgImage = document.createElement("div");
+  svgImage.classList.add("field__carrots-item-svg");
+  carrot.append(svgImage);
+
+  const ls = new LocalStorage("gridGarden");
+  const numberLvl = ls.get("numberLevel");
+
+  carrot.setAttribute(
+    "style",
+    `${RULES_GARDEN.CARROT_RULES[Number(numberLvl) - 1]}`
+  );
+  console.log(carrot);
+  svgImage.style.background = "url(./assets/images/grid-garden/carrot-two.svg)";
   field.append(carrot);
 };
 
