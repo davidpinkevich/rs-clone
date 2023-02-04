80a76f9c900b7635c1d30c8c2ee9002769193758
davidpinkevich <davidpinkevich95@gmail.com>
2023-02-04 20:50:13 +0300

    feat: add drow levels grid after choose lvl

---
+++ src/ts/pages/games/grid-garden/buttons/drow-levels.ts
@@ -0,0 +1,28 @@
+import addRules from "../start-game/rules-add";
+import createField from "../field-create/create-field";
+import addCode from "../start-game/code-add";
+import addCarrots from "../field-create/carrots-add";
+import addWater from "../field-create/water-add";
+import moveWater from "../field-create/move-water";
+import moveGarden from "../field-create/move-garden";
+
+const drowLevels = () => {
+  addRules();
+  createField();
+  addCode();
+  addCarrots();
+  addWater();
+  moveWater();
+  moveGarden();
+};
+
+export default drowLevels;
+
+// addRules();
+// createField(); возможно не надо
+// addCode();
+// addCarrots();
+// addWater();
+// moveWater();
+// moveGarden();
+// chooseLevel();
