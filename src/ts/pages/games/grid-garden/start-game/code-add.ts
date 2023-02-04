097b7c08fa73f54015161b020debd51ea7468919
davidpinkevich <davidpinkevich95@gmail.com>
2023-02-04 22:37:24 +0300

    feat: add code for garden on page

--- src/ts/pages/games/grid-garden/start-game/code-add.ts
+++ src/ts/pages/games/grid-garden/start-game/code-add.ts
@@ -1,9 +1,32 @@
 import RULES_GARDEN from "../data/rules";
+import LocalStorage from "../../../../utils/LocalStorage";
 
 const addCode = () => {
   const task = <HTMLElement>document.querySelector(".code__garden-task");
+  task.innerHTML = "";
+  const ls = new LocalStorage("gridGarden");
+  const numberLvl = ls.get("numberLevel");
 
-  task.insertAdjacentHTML("beforeend", RULES_GARDEN.CODE[1]);
+  if (Number(numberLvl) <= 10) {
+    task.insertAdjacentHTML("afterbegin", RULES_GARDEN.TEXT__GARDEN[0]);
+  } else {
+    task.insertAdjacentHTML(
+      "afterbegin",
+      RULES_GARDEN.TEXT__GARDEN[Number(numberLvl) - 10]
+    );
+  }
+  task.insertAdjacentHTML(
+    "beforeend",
+    RULES_GARDEN.TEXT__WATER[Number(numberLvl) - 1]
+  );
+
+  const lel: NodeListOf<HTMLElement> = document.querySelectorAll(
+    ".code__garden-task-text>span"
+  );
+  console.log(lel);
+  for (let i = 0; i < lel.length; i += 1) {
+    lel[i].innerHTML = `${`${i + 1}.`}`;
+  }
 };
 
 export default addCode;
