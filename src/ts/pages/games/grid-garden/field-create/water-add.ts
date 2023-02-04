0793ad8f8732ba3ddae244d946dba2f53c9c7531
davidpinkevich <davidpinkevich95@gmail.com>
2023-02-04 22:38:52 +0300

    feat: add drow water garden for next level

--- src/ts/pages/games/grid-garden/field-create/water-add.ts
+++ src/ts/pages/games/grid-garden/field-create/water-add.ts
@@ -1,11 +1,18 @@
 import RULES_GARDEN from "../data/rules";
+import LocalStorage from "../../../../utils/LocalStorage";
 
 const addWater = () => {
   const field = <HTMLElement>document.querySelector(".field__water");
 
   const water = document.createElement("div");
   water.classList.add("field__water-item");
-  water.setAttribute("style", `${RULES_GARDEN.WATER_RULES[1]}`);
+  const ls = new LocalStorage("gridGarden");
+  const numberLvl = ls.get("numberLevel");
+  // field.setAttribute("style", `${RULES_GARDEN.WATER_RULES[11]}`);  когда для garden по идее
+  water.setAttribute(
+    "style",
+    `${RULES_GARDEN.WATER_RULES[Number(numberLvl) - 1]}`
+  );
   field.append(water);
   console.log(water);
 };
