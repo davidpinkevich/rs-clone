44a8bf9535c69eaec0fcb134838bafbfee2d6b8a
Anton Vyun <zeta664433@gmail.com>
2023-02-04 22:32:56 +0300

    feat: add animation logic

--- src/ts/pages/games/css-surgeon/hide-levels.ts
+++ src/ts/pages/games/css-surgeon/hide-levels.ts
@@ -1,8 +1,13 @@
+import surgeonStore from "./data/surgeon-store";
+
 const hideLevels = () => {
   const holesEl: NodeListOf<HTMLElement> =
     document.querySelectorAll(".surgeon__holes");
   const itemsEl: NodeListOf<HTMLElement> =
     document.querySelectorAll(".surgeon__items");
+  const viewEl = <HTMLElement>document.querySelector(".surgeon__view");
+  const samEl = <HTMLElement>document.querySelector(".surgeon__sam");
+  const bodyEl = <HTMLElement>document.querySelector(".surgeon__body");
 
   holesEl.forEach((holeEl) => {
     holeEl.classList.add("surgeon__holes_hide");
@@ -11,6 +16,14 @@ const hideLevels = () => {
   itemsEl.forEach((itemEl) => {
     itemEl.classList.add("surgeon__items_hide");
   });
+
+  const { currentLevel } = surgeonStore;
+  const { origin } = surgeonStore.levels[currentLevel - 1];
+
+  viewEl.style.background = "";
+  samEl.style.transformOrigin = origin;
+  bodyEl.style.opacity = "1";
+  samEl.style.transform = "scale(1)";
 };
 
 export default hideLevels;
--- src/ts/pages/games/css-surgeon/show-level.ts
+++ src/ts/pages/games/css-surgeon/show-level.ts
@@ -5,15 +5,21 @@ const showLevel = () => {
     document.querySelectorAll(".surgeon__holes");
   const itemsEl: NodeListOf<HTMLElement> =
     document.querySelectorAll(".surgeon__items");
-  const bodyEl = <HTMLElement>document.querySelector(".surgeon__sam");
   const viewEl = <HTMLElement>document.querySelector(".surgeon__view");
+  const samEl = <HTMLElement>document.querySelector(".surgeon__sam");
+  const bodyEl = <HTMLElement>document.querySelector(".surgeon__body");
 
   const { currentLevel } = surgeonStore;
+  const { origin } = surgeonStore.levels[currentLevel - 1];
 
-  holesEl[currentLevel - 1].classList.remove("surgeon__holes_hide");
-  itemsEl[currentLevel - 1].classList.remove("surgeon__items_hide");
+  samEl.style.transformOrigin = origin;
+  samEl.style.transform = "scale(7)";
   viewEl.style.background = "#ffd4ce";
-  bodyEl.style.opacity = "0";
+  setTimeout(() => {
+    bodyEl.style.opacity = "0";
+    holesEl[currentLevel - 1].classList.remove("surgeon__holes_hide");
+    itemsEl[currentLevel - 1].classList.remove("surgeon__items_hide");
+  }, 2000);
 };
 
 export default showLevel;
