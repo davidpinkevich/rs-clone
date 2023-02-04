4b077f378924bb972283c10fdec42cf66b85a0cd
Anton Vyun <zeta664433@gmail.com>
2023-02-04 13:11:24 +0300

    feat: add create instruction

---
+++ src/ts/pages/games/css-surgeon/create-instruction.ts
@@ -0,0 +1,23 @@
+import surgeonStore from "./data/surgeon-store";
+
+const createInstruction = () => {
+  const instructionsEl = <HTMLElement>(
+    document.querySelector(".surgeon__instructions")
+  );
+  const editorEl = <HTMLElement>document.querySelector(".surgeon__editor");
+  const selectorEl = <HTMLElement>document.querySelector(".surgeon__before");
+
+  const { descr1, descr2, selector } =
+    surgeonStore.levels[surgeonStore.currentLevel - 1];
+
+  instructionsEl.innerHTML = `
+  <p>${descr1}</p>
+  <p>${descr2}</p>
+  `;
+
+  editorEl.style.display = "block";
+
+  selectorEl.innerHTML = `${selector} {`;
+};
+
+export default createInstruction;
