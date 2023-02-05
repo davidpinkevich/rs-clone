37e18f9f6372ba17c61200d8d545f2646f2b1b08
Anton Vyun <zeta664433@gmail.com>
2023-02-05 12:44:30 +0300

    feat: add code handler

---
+++ src/ts/pages/games/css-surgeon/code-handler.ts
@@ -0,0 +1,31 @@
+import Coordinates from "../../../utils/Coordinates";
+import surgeonStore from "./data/surgeon-store";
+
+const codeHandler = () => {
+  const codeEl = <HTMLElement>document.querySelector(".surgeon__code");
+
+  codeEl.addEventListener("input", (e) => {
+    const holesEl: NodeListOf<HTMLElement> =
+      document.querySelectorAll(".surgeon__holes");
+    const itemsEl: NodeListOf<HTMLElement> =
+      document.querySelectorAll(".surgeon__items");
+    const nextBtn = <HTMLButtonElement>document.querySelector(".button__next");
+    const { currentLevel } = surgeonStore;
+    const target = e.target as HTMLTextAreaElement;
+
+    itemsEl[currentLevel - 1].setAttribute("style", target.value);
+
+    const coord = new Coordinates(
+      itemsEl[currentLevel - 1],
+      holesEl[currentLevel - 1]
+    );
+
+    if (coord.comparisonСoordinates()) {
+      nextBtn.disabled = false;
+    } else {
+      nextBtn.disabled = true;
+    }
+  });
+};
+
+export default codeHandler;
