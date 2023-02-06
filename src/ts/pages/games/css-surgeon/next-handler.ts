83e3b7635bee3a20473dd4c84b1f6b67b6113886
Anton Vyun <zeta664433@gmail.com>
2023-02-06 15:34:49 +0300

    feat: update level switch

--- src/ts/pages/games/css-surgeon/next-handler.ts
+++ src/ts/pages/games/css-surgeon/next-handler.ts
@@ -1,17 +1,29 @@
 import createInstruction from "./create-instruction";
+import currentLevel from "./current-level";
 import surgeonStore from "./data/surgeon-store";
 import hideLevels from "./hide-levels";
+import showItem from "./show-item";
 import showLevel from "./show-level";
 
 const nextHandler = () => {
   const nextBtn = <HTMLButtonElement>document.querySelector(".button__next");
+  const codeEl = <HTMLTextAreaElement>document.querySelector(".surgeon__code");
 
   nextBtn.addEventListener("click", () => {
+    showItem();
+    const itemsEl: NodeListOf<HTMLElement> = document.querySelectorAll(
+      ".surgeon__item_hide"
+    );
     nextBtn.disabled = true;
+    codeEl.value = "";
     hideLevels();
-    surgeonStore.currentLevel += 1;
-    if (surgeonStore.currentLevel >= surgeonStore.levels.length) {
-      nextBtn.disabled = true;
+    currentLevel();
+    if (
+      surgeonStore.currentLevel === surgeonStore.levels.length &&
+      itemsEl.length === 0
+    ) {
+      currentLevel();
+      return;
     }
     createInstruction();
     setTimeout(() => {
