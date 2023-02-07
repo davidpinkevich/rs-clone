8ef0d33beba6b785c0a6c61cb4e62ee109a70c5b
Anton Vyun <zeta664433@gmail.com>
2023-02-07 12:44:31 +0300

    feat: create winner block

---
+++ src/ts/pages/games/css-surgeon/create-winner.ts
@@ -0,0 +1,21 @@
+const createWinner = () => {
+  const nextBtn = <HTMLButtonElement>document.querySelector(".button__next");
+  const instructionsEl = <HTMLElement>(
+    document.querySelector(".surgeon__instructions")
+  );
+  const editorEl = <HTMLElement>document.querySelector(".surgeon__editor");
+  const winnerEl = <HTMLElement>document.querySelector(".surgeon__winner");
+  const overlayEl = <HTMLElement>document.querySelector(".surgeon__overlay");
+  const samImg = <HTMLElement>document.querySelector(".surgeon__sam-img");
+  const samWinImg = <HTMLElement>document.querySelector(".surgeon__sam-win");
+
+  nextBtn.disabled = true;
+  instructionsEl.style.display = "none";
+  editorEl.style.display = "";
+  winnerEl.style.display = "block";
+  overlayEl.style.display = "none";
+  samImg.style.display = "none";
+  samWinImg.style.display = "block";
+};
+
+export default createWinner;
