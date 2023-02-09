2aa87bd8f97040a80e2bc09121dbd0be30594314
Anton Vyun <zeta664433@gmail.com>
2023-02-09 21:21:03 +0300

    feat: add volume icons

--- src/ts/pages/games/css-surgeon/handlers/volume-handler.ts
+++ src/ts/pages/games/css-surgeon/handlers/volume-handler.ts
@@ -3,14 +3,19 @@ const volumeHandler = () => {
     document.querySelector(".surgeon__audio-correct")
   );
   const volumeBtn = <HTMLElement>document.querySelector(".surgeon__volume");
+  const volumeImg = <HTMLImageElement>volumeBtn.querySelector("img");
 
   let volume = 1;
 
   volumeBtn.addEventListener("click", () => {
     if (volume) {
+      volumeImg.src =
+        "https://cdn0.iconfinder.com/data/icons/zondicons/20/volume-off-256.png";
       audioEl.volume = 0;
       volume = 0;
     } else {
+      volumeImg.src =
+        "https://cdn4.iconfinder.com/data/icons/font-awesome-2/2048/f028-512.png";
       audioEl.volume = 1;
       volume = 1;
     }
