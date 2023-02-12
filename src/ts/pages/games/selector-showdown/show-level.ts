5e6c812c3c152832feda811b2d444ba370bc2213
Anton Vyun <zeta664433@gmail.com>
2023-02-12 13:39:22 +0300

    feat: show level from store

---
+++ src/ts/pages/games/selector-showdown/show-level.ts
@@ -0,0 +1,53 @@
+import showdownStore from "./data/showdown-store";
+import randomNumbers from "./utils/random-numbers";
+
+const showLevel = () => {
+  const instructionsEl = <HTMLElement>(
+    document.querySelector(".showdown__instructions")
+  );
+  const panelTopEl = <HTMLElement>document.querySelector(".showdown__top");
+  const selectorTopEl = <HTMLElement>(
+    document.querySelector(".showdown__selector_top")
+  );
+  const specificityTopEl = <HTMLElement>(
+    document.querySelector(".showdown__specificity_top")
+  );
+  const panelBottomEl = <HTMLElement>(
+    document.querySelector(".showdown__bottom")
+  );
+  const selectorBottomEl = <HTMLElement>(
+    document.querySelector(".showdown__selector_bottom")
+  );
+  const specificityBottomEl = <HTMLElement>(
+    document.querySelector(".showdown__specificity_bottom")
+  );
+
+  const { currentLevel } = showdownStore;
+  const { descr, info } = showdownStore.levels[currentLevel - 1];
+  const [first, second] = randomNumbers(2);
+  const selector1 = info[first].selector;
+  const score1 = info[first].score;
+  const selector2 = info[second].selector;
+  const score2 = info[second].score;
+
+  const score1Html = score1
+    .split("")
+    .map((char) => `<span>${char}</span>`)
+    .join("");
+  const score2Html = score2
+    .split("")
+    .map((char) => `<span>${char}</span>`)
+    .join("");
+
+  instructionsEl.innerHTML = descr;
+
+  panelTopEl.setAttribute("data-score", score1);
+  selectorTopEl.innerHTML = selector1;
+  specificityTopEl.innerHTML = score1Html;
+
+  panelBottomEl.setAttribute("data-score", score2);
+  selectorBottomEl.innerHTML = selector2;
+  specificityBottomEl.innerHTML = score2Html;
+};
+
+export default showLevel;
