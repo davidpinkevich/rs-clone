0e4721f9b57de8310276c78a48a5ef290058e3d6
Anton Vyun <zeta664433@gmail.com>
2023-02-13 23:02:42 +0300

    feat: fn call

--- src/ts/pages/games/selector-showdown/ShowdownPage.ts
+++ src/ts/pages/games/selector-showdown/ShowdownPage.ts
@@ -1,5 +1,19 @@
 import Page from "../../abstract/page";
-import showLevel from "./show-level";
+import createLevels from "./level/create-levels";
+import bottomHandler from "./handlers/bottom-handler";
+import correctHandler from "./handlers/correct-handler";
+import incorrectHandler from "./handlers/incorrect-handler";
+import levelHandler from "./handlers/level-handler";
+import menuHandler from "./handlers/menu-handler";
+import modalHandler from "./handlers/modal-handler";
+import newHandler from "./handlers/new-handler";
+import nextHandler from "./handlers/next-handler";
+import prevHandler from "./handlers/prev-handler";
+import topHandler from "./handlers/top-handler";
+import volumeHandler from "./handlers/volume-handler";
+import readLs from "./ls/read-ls";
+import showLevel from "./level/show-level";
+import showdownUi from "./showdown-ui";
 
 class ShowdownPage extends Page {
   constructor(id: string) {
@@ -7,66 +21,24 @@ class ShowdownPage extends Page {
   }
 
   executeAfterRender(): void {
+    createLevels();
+    readLs();
     showLevel();
+    menuHandler();
+    levelHandler();
+    nextHandler();
+    prevHandler();
+    topHandler();
+    bottomHandler();
+    correctHandler();
+    incorrectHandler();
+    modalHandler();
+    newHandler();
+    volumeHandler();
   }
 
   render() {
-    this.container.innerHTML = `
-      <header class="header" style="background-color: #000">
-        <div class="__container">
-          <a href="#">click to main from showdown</a>
-        </div>
-      </header>
-      <main class="showdown">
-        <div class="showdown__wrapper">
-          <section class="showdown__sidebar">
-            <h1>SELECTOR SHOWDOWN</h1>
-            <div class="showdown__nav">
-              <button class="btn btn__prev" disabled><img src="./assets/images/showdown/arrow-left.svg"/></button>
-              <div class="showdown__counter">
-                <div class="showdown__indicator">
-                  <span class="showdown__current">1</span>
-                  <span class="showdown__slash">/</span>
-                  <span class="showdown__total">64</span>
-                  <span class="showdown__caret">▾</span>
-                </div>
-                <div class="showdown__levels-wrapper">
-                  <div class="showdown__levels">
-                    <button data-level="1" class="btn btn__level btn__level_current">1</button>
-                    <button data-level="2" class="btn btn__level">2</button>
-                    <button data-level="3" class="btn btn__level btn__level_locked" disabled>3</button>
-                  </div>
-                  <button class="btn btn__volume"><img src="./assets/images/froggy/sound-on.svg"/></button>
-                </div>
-              </div>
-              <button class="btn btn__next"><img src="./assets/images/showdown/arrow-right.svg"/></button>
-            </div>
-            <div class="showdown__instructions"></div>
-          </section>
-          <section class="showdown__main">
-            <div class="showdown__panel showdown__top">
-              <span class="showdown__selector showdown__selector_top"></span>
-              <span class="showdown__specificity showdown__specificity_top"></span>
-              <div class="showdown__versus">VS</div>
-              <div class="showdown__background showdown__background_top"></div>
-            </div>
-            <div class="showdown__panel showdown__bottom">
-              <div class="showdown__selector showdown__selector_bottom"></div>
-              <span class="showdown__specificity showdown__specificity_bottom"></span>
-              <div class="showdown__background showdown__background_bottom"></div>
-            </div>
-            <div class="showdown__btn showdown__btn_incorrect">
-              <h2>Incorrect!</h2>
-              <span>Level Down</span>
-            </div>
-            <div class="showdown__btn showdown__btn_correct">
-              <h2>Correct!</h2>
-              <span>Level Up</span>
-            </div>
-          </section>
-        </div>
-      </main>
-    `;
+    this.container.innerHTML = showdownUi();
     return this.container;
   }
 }
