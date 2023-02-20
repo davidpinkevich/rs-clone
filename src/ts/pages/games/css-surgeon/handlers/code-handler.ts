aac48dd247d88632b6ea3f31249ce28a523808c3
Anton Vyun <zeta664433@gmail.com>
2023-02-20 11:37:54 +0300

    feat: add types

--- src/ts/pages/games/css-surgeon/handlers/code-handler.ts
+++ src/ts/pages/games/css-surgeon/handlers/code-handler.ts
@@ -42,9 +42,9 @@ const codeHandler = (): void => {
     }
   });
 
-  codeEl.addEventListener("paste", (e) => {
-    e.preventDefault();
-  });
+  // codeEl.addEventListener("paste", (e) => {
+  //   e.preventDefault();
+  // });
 };
 
 export default codeHandler;
