0fd413a81fdec93cfd469aac09aa06eca7eb4d22
davidpinkevich <davidpinkevich95@gmail.com>
2023-02-22 14:42:09 +0300

    feat: add link for result

--- src/ts/pages/games/grid-garden/patterns/drow-result-grid.ts
+++ src/ts/pages/games/grid-garden/patterns/drow-result-grid.ts
@@ -5,7 +5,7 @@ const drowResult = () =>
     <div class="grid__result-body">
       <div class="grid__result-body-img"></div>
       <div class="grid__result-body-text">Congratulations! You are lucky to collect carrots: <span></span><span>/${LEVELS_GARDEN.END}</span></div>
-      <button class="grid__result-body-link">Total result</button>
+      <button class="grid__result-body-link"><a href="#statistics">Total result</a></button>
     </div>
   </div>`;
 
